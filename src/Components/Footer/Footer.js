b41cf069b011aeee87fe7660908238e9e2776711
Rushikesh7997 <pardhikar26@gmail.com>
2022-09-20 08:15:44 +0530

    new update

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import "./footer.css"
+
+function Footer() {
+  
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © 2021</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Rushikesh Pardhikar</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/Rushikesh7997"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/rushikesh-pardhikar/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
