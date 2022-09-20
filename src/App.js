58944f4325caa1f88c5e6d713bb0bf0ddd723a64
Rushikesh7997 <pardhikar26@gmail.com>
2022-09-20 09:44:39 +0530

    graph added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./Components/Header/Header"
 import Footer from "./Components/Footer/Footer"
 import Contact from "./Pages/Contact"
 import "./style.css";
+import { GithubStats } from "./Components/GitHubCard/GithubStats";
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
       <Home />
       <About />
       <Projects />
+      <GithubStats/>
       <Contact />
       <br/>
       <Footer/>
